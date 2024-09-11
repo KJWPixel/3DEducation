8ad84608e919d7bede57b7adab9397540c367d73
gf04\User <sbsgameas@gmail.com>
2024-09-11 19:50:15 +0900

    09-11-학원

--- 3DEducation/Assets/4_Behaviour Tree/CEnemyBT.cs
+++ 3DEducation/Assets/4_Behaviour Tree/CEnemyBT.cs
@@ -8,13 +8,17 @@ public class CEnemyBT : MonoBehaviour
 {
     NavMeshAgent mNavMeshAgent = null;
 
-    GameObject mTarget = null; //주인공 캐릭터
+    GameObject mTarget = null;  //주인공 캐릭터
 
-    [SerializeField] bool mIsGrenade = false;    //수류탄 소지 여부
+    [SerializeField]
+    bool mIsGrenade = false;        //수류탄 소지 여부
 
-    [SerializeField] Vector3 mSpawnAnyPosition = Vector3.zero; //원래 지점
+    [SerializeField]
+    Vector3 mSpawnAnyPostion = Vector3.zero;        //원래 지점
 
-    [SerializeField] float mDetectedRadius = 0f; //탐지 반경
+    [SerializeField]
+    float mDetectedRadius = 0f;     //탐지 반경
+
 
     Sequence mRootNode = null;
 
@@ -23,28 +27,28 @@ public class CEnemyBT : MonoBehaviour
     void BuildBT()
     {
         //행동트리 구축 Build BT
-        //행동트리로 작성하면 코드 변경의 경계가 행동트리 구축 부분으로 국지화 된다.
+        //행동트리로 작성하면 코드 변경의 경계가 행동트리 구축 부분으로 국지화된다.
 
-        //Level4
-        ActionNode tANAvoid = new ActionNode(DoIsAvoid);
-        ActionNode tANFollow = new ActionNode(DoIsFollow);
+        //level 4
+        ActionNode tANAvoid = new ActionNode(DoAvoid);
+        ActionNode tANFollow = new ActionNode(DoFollow);
 
-        //Level3
-        //Level 3_0
+        //level 3
+        //level 3_0
         ActionNode tANIsGrenade = new ActionNode(DoIsGrenade);
         Inverter tNotAvoid = new Inverter(tANAvoid);
         List<Node> tLevel_3_0 = new List<Node>();
         tLevel_3_0.Add(tANIsGrenade);
         tLevel_3_0.Add(tNotAvoid);
 
-        //Level 3_1
+        //level 3_1
         ActionNode tANIsArrived = new ActionNode(DoIsArrived);
         Inverter tNotFollow = new Inverter(tANFollow);
         List<Node> tLevel_3_1 = new List<Node>();
         tLevel_3_1.Add(tANIsArrived);
-        tLevel_3_1.Add(tANFollow);
+        tLevel_3_1.Add(tNotFollow);
 
-        //Level2
+        //level 2
         Selector tSelectIsGrenade = new Selector(tLevel_3_0);
         Selector tSelectIsArrived = new Selector(tLevel_3_1);
         ActionNode tANThrowGrenade = new ActionNode(DoThrowGrenade);
@@ -53,13 +57,14 @@ public class CEnemyBT : MonoBehaviour
         tLevel_2.Add(tSelectIsArrived);
         tLevel_2.Add(tANThrowGrenade);
 
-        //Level1
+        //level 1
         mRootNode = new Sequence(tLevel_2);
     }
 
+
     NodeStates DoIsGrenade()
     {
-        if(mIsGrenade)
+        if (mIsGrenade)
         {
             Debug.Log("Is Grenade? TRUE");
             return NodeStates.SUCCESS;
@@ -69,13 +74,12 @@ public class CEnemyBT : MonoBehaviour
             return NodeStates.FAILURE;
         }
     }
-
     NodeStates DoIsArrived()
     {
         //탐지 범위 안에 대상이 있다면 성공
         if (Vector3.Distance(this.transform.position, mTarget.transform.position) <= mDetectedRadius)
         {
-            Debug.Log("<color='yellow'>Is Arrived, TRUE!!!</color>");
+            Debug.Log("<color='yellow'>Is Arrived, TRUE!!!!!!!</color>");
 
             return NodeStates.SUCCESS;
         }
@@ -84,27 +88,27 @@ public class CEnemyBT : MonoBehaviour
             return NodeStates.FAILURE;
         }
     }
-
-    NodeStates DoIsAvoid()
+    NodeStates DoAvoid()
     {
         Debug.Log("DoAvoid");
-        mNavMeshAgent.SetDestination(mSpawnAnyPosition);
+        mNavMeshAgent.SetDestination(mSpawnAnyPostion);
+
         return NodeStates.SUCCESS;
     }
-
-    NodeStates DoIsFollow()
+    NodeStates DoFollow()
     {
         Debug.Log("DoFollow");
         mNavMeshAgent.SetDestination(mTarget.transform.position);
+
         return NodeStates.SUCCESS;
     }
     NodeStates DoThrowGrenade()
     {
-        Debug.Log("<size=16><color='red'>DoThrowGrenade</color></size>");
+        Debug.Log("<size='16'><color='red'>Do Throw Grenade</color></size>");
 
         //수류탄은 1발이라고 가정
         mIsGrenade = false;
-        
+
         return NodeStates.SUCCESS;
     }
 
@@ -113,18 +117,21 @@ public class CEnemyBT : MonoBehaviour
         mNavMeshAgent = GetComponent<NavMeshAgent>();
     }
 
+    // Start is called before the first frame update
     void Start()
     {
         //하이어라키를 검색하여 목적대상을 찾음.
         mTarget = FindObjectOfType<CPChar_step_1>().gameObject;
         //원래 지점도 기억시켜 둔다.
-        mSpawnAnyPosition = this.transform.position;
+        mSpawnAnyPostion = this.transform.position;
 
 
         //행동트리 구축
         BuildBT();
+
     }
 
+    // Update is called once per frame
     void Update()
     {
         mRootNode.Evaluate();
@@ -133,11 +140,12 @@ public class CEnemyBT : MonoBehaviour
     private void OnDrawGizmos()
     {
         //목적지점 확인용 기즈모
-        //Gizmos.color = Color.yellow;
+        //Gizmos.color = Color.red;
         //Gizmos.DrawWireCube(mTarget.transform.position, Vector3.one * 0.5f);
 
+
         //탐지범위 확인용 기즈모
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(this.transform.position, mDetectedRadius);
     }
 }
---
+++ 3DEducation/Assets/5_TPS_Light/Scripts/CEnemyThrow.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class CEnemyThrow : MonoBehaviour
+{
+    [SerializeField] GameObject PFGrenade = null;
+
+    //발사 시작지점
+    [SerializeField] GameObject mPosFire = null;
+
+    //탄착지점(목표지점)
+    [SerializeField] GameObject mTarget = null;
+
+    public void DoFire()//DoFire(GameObject tTarget)
+    {
+        //주인공 캐릭터에게 던지는 것으로 가정
+        mTarget = FindObjectOfType<CPChar_step_1>().gameObject;
+        //mTarget = tTarget;
+
+        //test
+        //mTarget.transform.position = new Vector3(10, 0, 10);
+
+        //탄착지점 포물선 궤적을 그리는 속력 구하기 알고리즘
+        //각도는 45도 가정하자.
+
+        //zx평면에서의 벡터를 구하자.
+        Vector3 tVelocity = Vector3.zero;
+
+        //목표지점
+        Vector3 tTargetPos = mTarget.transform.position;
+        tTargetPos.y = 0f; //<-- zx평면만 가정했으므로
+        //시작지점
+        Vector3 tStartPos = mPosFire.transform.position;
+        tStartPos.y = 0f;
+
+        Vector3 tZXVector = (tTargetPos - tStartPos);
+
+        //45도 가정
+        tVelocity = (tZXVector.normalized + Vector3.up).normalized;//순수한 방향벡터를 구함
+
+        //크기
+        //45도를 가정한 초기속력
+        //zx평면에서의 시점과 종점 사이의 거리
+        float tDZX = tZXVector.magnitude;
+
+        //y축에서 시점과 종점사이의 거리
+        float tDY = tTargetPos.y - tStartPos.y;//일단 0
+        
+
+        float tCos = Mathf.Cos(45f * Mathf.Deg2Rad);
+        float tSin = Mathf.Sin(45f * Mathf.Deg2Rad);
+        float tTan = tSin / tCos;// y변화량/x변화량
+
+        //초기 속력 결정
+        float tScalarSpeed = (tDZX / tCos) * Mathf.Sqrt((-1) * 9.8f / (2f * (tDY - tTan * tDZX)));
+
+        //크기까지 적용하여 속도 결정
+        tVelocity = tVelocity * tScalarSpeed;
+
+        GameObject tGrenade = Instantiate<GameObject>(PFGrenade, mPosFire.transform.position, Quaternion.identity);
+
+        tGrenade.GetComponent<Rigidbody>().AddForce(tVelocity, ForceMode.Impulse);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(0,0,100,100), "test Throw"))
+        {
+            DoFire();
+            //DoFire(mTarget);
+            //DoFire()
+        }
+    }
+}
